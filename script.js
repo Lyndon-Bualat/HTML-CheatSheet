6358dee41731b37523c5e598655d1ccb3b61c0c7
Lyndon-Bualat <64567818+Lyndon-Bualat@users.noreply.github.com>
2020-12-11 11:08:34 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,243 @@
+
+
+//  -------------------Tags------------------
+document.getElementById("tag1").placeholder =
+("Write notes here then save it as an Email" +"\n\n\n"+
+"<html> </html>" +"\n"+
+"Creates an HTML document" +"\n\n"+
+"<head> </head>" +"\n"+
+"Sets off the title & other info that isn't displayed" +"\n\n"+
+"<body> </body>" +"\n"+
+"Sets off the visible portion of the document" +"\n\n"+
+"<title> </title>"+"\n"+
+"Puts name of the document in the title bar; when" +"\n"+
+"bookmarking pages, this is what is bookmarked");
+
+document.getElementById("tag2").placeholder =
+("Write notes here then save it as an Email" +"\n\n\n"+
+"<pre> </pre>" +"\n"+
+"Creates preformatted text" +"\n\n"+
+"<h1> </h1> to <h6> </h6>" +"\n"+
+"Creates headlines" +"\n\n"+
+"<b> </b>" +"\n"+
+"Creates bold text" +"\n\n"+
+"<i> </i>"+"\n"+
+"Creates italicized text" +"\n\n"+
+"<tt> </tt>" +"\n"+
+"typewriter style text"  +"\n\n"+
+"<code> </code>" +"\n"+
+"Can put code here" +"\n\n"+
+"<cite> </cite>" +"\n"+
+"Creates a citation in italics" +"\n\n"+
+"<address> </address>" +"\n"+
+"Creates address section in italics" +"\n\n"+
+"<em> </em>" +"\n"+
+"Emphasizes by bolding the words" +"\n\n"+
+"<strong> </strong>" +"\n"+
+"Bolds the word"  +"\n\n"+
+"<font size=?> </font>" +"\n"+
+"Sets font size" +"\n\n"+
+"<font color=?> </font>" +"\n"+
+"Sets font color" +"\n\n"+
+"<font face=?> </font>" +"\n"+
+"Defines the font used"
+);
+
+document.getElementById("tag3").placeholder =
+("Write notes here then save it as an Email" +"\n\n\n"+
+
+"<input type=email name=?>" +"\n"+
+" Sets a single-line textbox for email addresses" +"\n\n"+
+"<input type=url name=?>" +"\n"+
+" Sets a single-line textbox for URLs" +"\n\n\n"+
+"<input type=number name=?>" +"\n"+
+" Sets a single-line textbox for a number" +"\n\n\n"+
+"<input type=range name=?>" +"\n"+
+" Sets a single-line text box for a range of numbers" +"\n\n\n"+
+"<input type=date/month/week/time name=?>" +"\n"+
+" Sets a single-line text box with a calendar" +"\n"+
+" showing the date/month/week/time" +"\n\n\n"+
+"<input type=search name=?>" +"\n"+
+" Sets a single-line text box for searching" +"\n\n\n"+
+"<input type=color name=?>" +"\n"+
+" Sets a single-line text box for picking a color"
+);
+// ------------------Tags----------------------
+
+
+// ------------------Table----------------------
+document.getElementById("table1").placeholder =
+("Write notes here then save it as an Email" +"\n\n\n"+
+"<table border=?>" +"\n"+
+"Sets the width of the border around table cells" +"\n\n\n"+
+"<table cellspacing=?>" +"\n"+
+"Sets amount of space between table cells" +"\n\n\n"+
+"<table cellpadding=?>" +"\n"+
+"Sets amount of space between a cell's border and its contents" +"\n\n\n"+
+"<table width=?>" +"\n"+
+"Sets width of the table in pixels or as a percentage" +"\n\n\n"+
+"<tr align=?>" +"\n"+
+"Sets alignment for cells within the row (left/center/right)"  +"\n\n\n"+
+"<td align=?>" +"\n"+
+"Sets alignment for cells (left/center/right)" +"\n\n\n"+
+"<tr valign=?>" +"\n"+
+"Sets vertical alignment for cells within the row (top/middle/bottom)"  +"\n\n\n"+
+"<td valign=?>" +"\n"+
+"Sets vertical alignment for cell (top/middle/bottom)" +"\n\n\n"+
+"<td rowspan=?>" +"\n"+
+"Sets number of rows a cell should span (default=1)" +"\n\n\n"+
+"<td colspan=?>" +"\n"+
+"Sets number of columns a cell should span" +"\n\n\n"+
+"<td nowrap>" +"\n"+
+"Prevents lines within a cell from being broken to fit"
+);
+
+document.getElementById("table2").placeholder =
+("Write notes here then save it as an Email" +"\n\n\n"+
+"<table> </table>" +"\n"+
+"Creates a table"  +"\n\n\n"+
+"<tr> </tr>" +"\n"+
+"Sets off each row in a table"  +"\n\n\n"+
+"<td> </td>" +"\n"+
+"Sets off each cell in a row"  +"\n\n\n"+
+"<th> </th>" +"\n"+
+"Sets off the table header (a normal cell with bold,centered text)"
+);
+//------------------Formating---------------------------
+
+document.getElementById("format1").placeholder =
+("Write notes here then save it as an Email" +"\n\n\n"+
+"<p> </p>" +"\n"+
+"Creates a new paragraph" +"\n\n\n"+
+"<br>" +"\n"+
+"AInserts a line break (carriage return)" +"\n\n\n"+
+"<blockquote> </blockquote>" +"\n"+
+"Puts content in a quote - indents text from both sides" +"\n\n\n"+
+"<div> </div>" +"\n"+
+"Used to format block content with CSS" +"\n\n\n"+
+"<span> </span>" +"\n"+
+"Used to format inline content with CSS"
+);
+
+// ---------------------list-------------------------
+
+document.getElementById("list1").placeholder =
+("Write notes here then save it as an Email" +"\n\n\n"+
+"<ul> </ul>" +"\n"+
+"Creates an unordered list" +"\n\n\n"+
+"<ol> </ol>" +"\n"+
+"Creates an ordered list" +"\n\n\n"+
+"<li> </li>" +"\n"+
+"Encompasses each list item" +"\n\n\n"+
+"<dl> </dl>" +"\n"+
+"Creates a definition list" +"\n\n\n"+
+"<dt>" +"\n"+
+" Precedes each defintion term" +"\n\n\n"+
+"<dd>" +"\n"+
+" Precedes each defintion"
+);
+// ----------------Forms---------------------
+document.getElementById("form1").placeholder =
+("Write notes here then save it as an Email" +"\n\n\n"+
+"<form> </form>"  +"\n"+
+"Creates a form section"  +"\n\n\n"+
+"<select name=?> </select>" +"\n"+
+"Creates a pulldown menu"  +"\n\n\n"+
+"<option>"  +"\n"+
+"Sets off each menu item"  +"\n\n\n"+
+"<textarea name=?></textarea>"  +"\n"+
+"Creates a text box area."  +"\n\n\n"+
+"<input type=checkbox name=? value=?>"  +"\n"+
+"Creates a checkbox."  +"\n\n\n"+
+"<input type=radio name=? value=?>"  +"\n"+
+"Creates a radio button.."  +"\n\n\n"+
+"<input type=text name=? size=?>"  +"\n"+
+"Creates a one-line text area." +"\n\n\n"+
+"<input type=submit value=?>"  +"\n"+
+"Creates a submit button."  +"\n\n\n"+
+"<input type=image name=? src=? border=? alt=?>"  +"\n"+
+"Creates a submit button using an image."  +"\n\n\n"+
+"<input type=reset>"  +"\n"+
+"Creates a reset button"
+);
+
+// ----------------Graphics---------------------
+document.getElementById("graphic1").placeholder =
+("Write notes here then save it as an Email" +"\n\n\n"+
+"<hr>" +"\n"+
+"Inserts a horizontal rule" +"\n\n\n"+
+" " +"\n"+
+"Copy and paste any icons" +"\n\n\n"+
+"<img src=URL />" +"\n"+
+"Add imges, can change by adding width and height attribute"
+);
+
+//----------------Links------------------------
+document.getElementById("link1").placeholder =
+("Write notes here then save it as an Email" +"\n\n\n"+
+"<a href=URL>clickable text</a>" +"\n"+
+"Creates a hyperlink to a Uniform Resource Locator"  +"\n\n\n"+
+"<a href=mailto:EMAIL_ADDRESS>clickable text</a>" +"\n"+
+"Creates a hyperlink to an email address"  +"\n\n\n"+
+"<a name=NAME>" +"\n"+
+"Creates a target location within a document"  +"\n\n\n"+
+"<a href=NAME>clickable text</a>" +"\n"+
+"Creates a link to that target location"
+);
+
+
+
+
+// ------------------buttons---------------------
+document.getElementById("home").onclick = home;
+function home() {
+  window.open(" ");
+}
+
+document.getElementById("cssLink").onclick = cssLink;
+function cssLink() {
+  window.open(" ");
+}
+
+document.getElementById("myBtn1").onclick = myBtn1;
+function myBtn1() {
+  window.open("https://codepen.io/NodNyLRB/embed/gOwLmjK");
+}
+
+document.getElementById("myBtn2").onclick = myBtn2;
+function myBtn2() {
+  window.open("https://codepen.io/NodNyLRB/embed/OJRbpGa");
+}
+
+document.getElementById("myBtn3").onclick = myBtn3;
+function myBtn3() {
+  window.open("https://codepen.io/NodNyLRB/embed/bGwBWNy");
+}
+
+document.getElementById("myBtn4").onclick = myBtn4;
+function myBtn4() {
+  window.open("https://codepen.io/NodNyLRB/embed/WNGojre");
+}
+
+
+document.getElementById("myBtn5").onclick = myBtn5;
+function myBtn5() {
+  window.open("https://codepen.io/NodNyLRB/embed/NWRbjxQ");
+}
+
+
+document.getElementById("myBtn6").onclick = myBtn6;
+function myBtn6() {
+  window.open("https://codepen.io/NodNyLRB/embed/YzGpVGj");
+}
+
+document.getElementById("myBtn7").onclick = myBtn7;
+function myBtn7() {
+  window.open("https://codepen.io/NodNyLRB/embed/NWRbgYv");
+}
+
+
+document.getElementById("restart").onclick = restart;
+function restart() {
+  location.reload();
+}
